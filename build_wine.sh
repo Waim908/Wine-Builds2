a6c41647ed034445f3385a94f1e0472793cec10c
hostei <hostei@qq.com>
2025-06-15 09:00:01 +0800

    hsjs

--- build_wine.sh
+++ build_wine.sh
@@ -208,6 +208,7 @@ if [ -n "${CUSTOM_SRC_PATH}" ]; then
 elif [ "$WINE_BRANCH" = "staging-tkg" ] || [ "$WINE_BRANCH" = "staging-tkg-ntsync" ]; then
 	if [ "$WINE_BRANCH" = "staging-tkg" ] && [ "${EXPERIMENTAL_WOW64}" = "true" ]; then
 		git clone https://github.com/hostei33/wine-tkg wine -b glibc10.10
+		
 	else
 		if [ "$WINE_BRANCH" = "staging-tkg" ]; then
 			git clone https://github.com/Kron4ek/wine-tkg wine
